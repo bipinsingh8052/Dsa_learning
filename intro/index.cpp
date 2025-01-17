05ab593fc1a91446c46e65ce213cfa99c682674d
Bipin Singh <bipinsingh8052@gmail.com>
2025-01-17 17:11:55 +0530

    bu

--- intro/index.cpp
+++ intro/index.cpp
@@ -1,1 +1,32 @@
-git 
+data type 
+// when you can store a data in is called a data type
+2type of data type
+1.primitive
+2.non-primitive
+
+
+1.primitive 
+// pre define data type is called primitive data type (char ,int ,float,bigint,string)
+2.non primitive
+// is called user-define (array,class)
+two type of non-primitive data type
+1.linear data type
+2.non-linear data type
+
+
+1.linear datatype
+(Linklist,Quence,stack,class,array)
+
+2.non-liner datatype
+{tree,graph}
+
+
+
+what is Time complexity
+// 
+
+
+
+
+what is space complexity
+// 
